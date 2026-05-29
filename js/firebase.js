442518c709a2eab99a41b3e6f1acce919646afa3
CobraCom-ux <cobracommunicationllc@gmail.com>
2026-05-29 09:07:20 -0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjqLcLVuJYqF-XgQjc5bqg_ui2Axxp1fM",
+  authDomain: "hrms-853b5.firebaseapp.com",
+  projectId: "hrms-853b5",
+  storageBucket: "hrms-853b5.firebasestorage.app",
+  messagingSenderId: "379372914724",
+  appId: "1:379372914724:web:9608dfc493aff3eb1b70b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
