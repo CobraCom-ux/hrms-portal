e5bb2754ecbbe95169f658af0c21265d6fcfe9ef
CobraCom-ux <cobracommunicationllc@gmail.com>
2026-05-29 09:38:58 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,4 @@
 import { auth, db } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-export async function login(email, password) {
-  const userCred = await signInWithEmailAndPassword(auth, email, password);
-
-  const uid = userCred.user.uid;
-  const snap = await getDoc(doc(db, "users", uid));
-
-  if (snap.exists()) {
-    return snap.data();
-  } else {
-    throw new Error("User not found");
-  }
-}
-import { auth, db } from "./firebase.js";
 
 import {
   signInWithEmailAndPassword
